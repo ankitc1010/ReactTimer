2b96d9e0ada4a95799a771abbdbdaf1590862c43
ankitc1010 <ankit.c1010@gmail.com>
2017-02-06 11:07:09 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var expressStaticGzip = require("express-static-gzip");
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
@@ -15,6 +15,6 @@ app.use("/", expressStaticGzip("public"));
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
+app.listen(PORT, process.env.IP, function () {
   console.log('Express server is up on port ' + PORT);
 });
